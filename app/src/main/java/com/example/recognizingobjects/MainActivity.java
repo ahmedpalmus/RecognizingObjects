5959cc80f966eae5a140ce2dd2c0ffb833e75075
admin <ahmed.g@live.com>
2023-04-09 23:41:34 +0300

    new

--- app/src/main/java/com/example/recognizingobjects/MainActivity.java
+++ app/src/main/java/com/example/recognizingobjects/MainActivity.java
@@ -2,13 +2,90 @@ package com.example.recognizingobjects;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    EditText user, password;
+    String username, pass;
+
+    String URL = Server.ip + "login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user = findViewById(R.id.log_user);
+        password = findViewById(R.id.log_pass);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        user.setText("");
+        password.setText("");
+    }
+
+    public void logClick(View v) {
+        if (v.getId() == R.id.log_log) {
+            username = user.getText().toString().trim();
+            pass = password.getText().toString().trim();
+
+            if (username.length() > 3) {
+                login();
+            } else {
+                Toast.makeText(this, getResources().getString(R.string.loginError), Toast.LENGTH_LONG).show();
+            }
+        } else if (v.getId() == R.id.log_reg) {
+            Intent intent = new Intent(MainActivity.this, Register.class);
+            startActivity(intent);
+        }
+    }
+
+    private void login() {
+        class RegAsync extends AsyncTask<String, Void, String> {
+            private Dialog loadingDialog;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loadingDialog = ProgressDialog.show(MainActivity.this, getResources().getString(R.string.wait), getResources().getString(R.string.connecting));
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                Connection con = new Connection();
+                HashMap<String, String> data = new HashMap<>();
+                data.put("username", params[0]);
+                data.put("password", params[1]);
+                String result = con.sendPostRequest(URL, data);
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                loadingDialog.dismiss();
+                if (result.isEmpty() || result.equals("Error"))
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.check), Toast.LENGTH_LONG).show();
+                else if (result.equals("failure")) {
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.userExist), Toast.LENGTH_LONG).show();
+                } else if (result.equalsIgnoreCase("success")) {
+                    Intent intent = new Intent(MainActivity.this, UserPage.class);
+                    intent.putExtra("id", username);
+                    startActivity(intent);
+                }
+            }
+        }
+        RegAsync la = new RegAsync();
+        la.execute(username, pass);
+    }
+
 }
---
+++ app/src/main/java/com/example/recognizingobjects/Server.java
@@ -0,0 +1,6 @@
+package com.example.recognizingobjects;
+
+public class Server {
+    public static String ip ="http://10.0.2.2/recog_object/";
+    //public static String ip ="http://192.168.1.227/recog_object/";
+}
